c612892161313537e5caa10cc9b931d5173a688b
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-13 15:08:04 -0700

    cleans up files

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,9 @@
 require 'bundler'
 Bundler.require
 
-require 'sinatra/base'
-require 'sinatra/activerecord'
+# require 'sinatra/base'
+# require 'sinatra/activerecord'
 
 require_all './app'
-
-# require './app/models/pirate.rb'
-# require './app/models/ship.rb'
 
 set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
