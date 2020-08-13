5823427f435a8a86fa84ed07a6f4d860cf94392d
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-13 13:01:42 -0700

    creates migration for creating the pirates tables

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,8 @@ require 'bundler'
 Bundler.require
 
 require 'sinatra/base'
+require 'sinatra/activerecord'
 
 require_all './app'
 
+set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
