a664a9310ad5e8db00568969da85ce5bcf1d0d6c
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-13 14:11:46 -0700

    updates views and controller

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -9,5 +9,21 @@ class Application < Sinatra::Base
 
         erb :'/pirates/index'
     end
+    
+    get '/pirates/new' do
+        erb :'/pirates/new'
+    end 
+
+    get '/pirates/:id' do 
+        @pirate = Pirate.find(params[:id])
+        
+        erb :'/pirates/show'
+    end 
+
+    post '/pirates' do 
+        pirate = Pirate.new(name: params[:pirate][:name], weight: params[:pirate][:weight], height: params[:pirate][:weight])
+        
+        # params[:pirate][:ships].each do |ship_detail|
+    end
 
 end 
